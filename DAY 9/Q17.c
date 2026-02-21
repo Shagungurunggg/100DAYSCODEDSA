11c9566a3f2dafdc7f2de51846a062545b57d0f7
Shagungurunggg <shagungurung03@gmail.com>
2026-02-21 08:31:27 +0530

    Create Q17.c

---
+++ DAY 9/Q17.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1001];
+    scanf("%s", s);
+
+    int n = strlen(s);
+
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%c", s[i]);
+    }
+
+    return 0;
+}
