d236826df02d96f6ac5b7abf38d7779539ee4a50
Shagungurunggg <shagungurung03@gmail.com>
2026-03-04 20:02:11 +0530

    Create Q26.c
    Implement a doubly linked list with insertion and traversal functionality using dynamic memory allocation.

---
+++ DAY 26/Q26.c
@@ -0,0 +1,42 @@
+Doubly Linked List Insertion and Traversal - Implement using linked list with dynamic memory allocation.
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure for doubly linked list
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+int main() {
+    int n, val;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &val);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = val;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+
+        if(head == NULL) {
+            head = temp = newNode; 
+        } else {
+            temp->next = newNode;
+            newNode->prev = temp;
+            temp = newNode;
+        }
+    }
+  
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
