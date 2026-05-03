afc61408ba4e1bbe4c7a78a3b9ea7645539570fa
Shagungurunggg <shagungurung03@gmail.com>
2026-05-03 16:40:19 +0530

    Create Q47.c

---
+++ DAY 47/Q47.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree Node
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+// Queue for tree construction
+struct Queue {
+    struct Node* arr[1000];
+    int front, rear;
+};
+
+// Create new node
+struct Node* newNode(int val) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = val;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// Queue functions
+void initQueue(struct Queue* q) {
+    q->front = q->rear = 0;
+}
+
+void enqueue(struct Queue* q, struct Node* node) {
+    q->arr[q->rear++] = node;
+}
+
+struct Node* dequeue(struct Queue* q) {
+    return q->arr[q->front++];
+}
+
+int isEmpty(struct Queue* q) {
+    return q->front == q->rear;
+}
+
+// Build tree from level-order input (-1 = NULL)
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* root = newNode(arr[0]);
+    struct Queue q;
+    initQueue(&q);
+    enqueue(&q, root);
+
+    int i = 1;
+
+    while (i < n && !isEmpty(&q)) {
+        struct Node* curr = dequeue(&q);
+
+        if (i < n && arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            enqueue(&q, curr->left);
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            enqueue(&q, curr->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Find height
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+// Main
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    printf("%d", height(root));
+
+    return 0;
+}
